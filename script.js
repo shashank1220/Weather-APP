c6489552a2c711deddf2fd3342e3615b2557a6f3
SHASHANK PANDEY <shashank2012pandey@gmail.com>
2025-04-17 21:50:51 +0530

    Update script.js
    - Added functionality to display the weather name.
    - Dynamically change the card color based on the current weather condition.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 
+const card = document.querySelector(".card");
+
 const weather = document.querySelector(".weather");
 const error = document.querySelector(".error");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -25,24 +27,33 @@ async function checkWeather(city) {
     
             if ( data.weather[0].main == "Clouds"){
                 weatherIcon.src = "clouds.png";
+                card.style.background = "#808080";
+                document.querySelector(".weather-name").innerText = "Cloudy";
             }
             else if ( data.weather[0].main == "Rain"){
                 weatherIcon.src = "rain.png";
-            }
-            else if ( data.weather[0].main == "Wind"){
-                weatherIcon.src = "wind.png";
+                card.style.background = "#28628f";
+                document.querySelector(".weather-name").innerText = "Rainy";
             }
             else if ( data.weather[0].main == "Mist"){
                 weatherIcon.src = "mist.png";
+                card.style.background = "#a5c0c2";
+                document.querySelector(".weather-name").innerText = "Mist";
             }
             else if ( data.weather[0].main == "Snow"){
                 weatherIcon.src = "snow.png";
+                card.style.background = "#7dcef7";
+                document.querySelector(".weather-name").innerText = "Snowy";
             }
             else if ( data.weather[0].main == "Drizzle"){
                 weatherIcon.src = "drizzle.png";
+                card.style.background = "#8dafac";
+                document.querySelector(".weather-name").innerText = "Dizzling";
             }
             else if ( data.weather[0].main == "Clear"){
                 weatherIcon.src = "clear.png";
+                card.style.background = "#009bfc";
+                document.querySelector(".weather-name").innerText = "Clear";
             }
             error.style.display = "none";
             weather.style.display = "block";
